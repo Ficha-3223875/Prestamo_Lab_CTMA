c72fdd22fa9c49a111cbbf97a809418a9c8fadeb
Maria Paula <mariapaulaherrerarivas@gmail.com>
2026-09-08 11:11:09 -0500

    build: actualizar a Gradle 9.1, AGP 9.0.1 y Kotlin integrado para soportar JDK 25

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -28,16 +28,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-
     buildFeatures {
         compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
